e7dd4ebb0db06fe0ca0ea835885fe0673eef2db0
Cameron Cuzmanko <camcuz1@gmail.com>
2016-06-29 09:31:21 -0700

    character count initiated

--- app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
@@ -3,6 +3,8 @@ package com.codepath.apps.mysimpletweets;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -23,6 +25,7 @@ public class ComposeActivity extends AppCompatActivity {
     User user;
     Tweet tweet;
     EditText etTweet;
+    TextView tvChars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,26 @@ public class ComposeActivity extends AppCompatActivity {
                 //My current user account's information
                 getSupportActionBar().setTitle("@" + user.getScreenName());
                 populateComposeHeader(user);
+            }
+        });
+        etTweet = (EditText) findViewById(R.id.etTweet);
+        tvChars = (TextView) findViewById(R.id.tvChars);
+        etTweet.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+                tvChars.setText(String.valueOf(140-s.length()));
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
             }
         });
     }
@@ -53,7 +76,6 @@ public class ComposeActivity extends AppCompatActivity {
 //    }
 
     public void onPostTweet(View view){
-        etTweet = (EditText) findViewById(R.id.etTweet);
         String post = etTweet.getText().toString();
         client.postStatus(post, new JsonHttpResponseHandler() {
             @Override
